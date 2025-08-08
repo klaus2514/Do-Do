28fa1e2422f906cdc820ebde86155938084743d3
Manjeet Sharan <147024105+klaus2514@users.noreply.github.com>
2025-08-08 21:47:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://do-do-sable.vercel.app/",
+  origin: "https://do-do-sable.vercel.app",
 }));
 
 app.use("/api", chatRoutes);
